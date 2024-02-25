40a7e3faba4ad403a5977952db9fa7945bb5020a
Michal Viwegier <beebas@wp.pl>
2024-02-26 00:35:07 +0100

    Refactor - done changed "class components" into "function components"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,30 +24,6 @@ export const App = () => {
   const [bigPictureUrl, setBigPictureUrl] = useState('');
   const [bigPictureTags, setBigPictureTags] = useState('');
 
-  // const [state, setState] = useState({
-  // actualPage: 1,
-  // pictures: [],
-  // isModal: false,
-  // isLoader: false,
-  // searchQuery: '',
-  // totalHits: 0,
-  // totalPages: 0,
-  // bigPictureUrl: '',
-  // bigPictureTags: '',
-  // });
-
-  // const {
-  //   actualPage,
-  //   pictures,
-  //   isModal,
-  //   isLoader,
-  //   searchQuery,
-  //   totalHits,
-  //   totalPages,
-  //   bigPictureUrl,
-  //   bigPictureTags,
-  // } = state;
-
   const handleSubmitForm = query => {
     window.scrollTo(0, 0);
     if (query.trim() === '') {
@@ -66,21 +42,11 @@ export const App = () => {
 
     async function getData() {
       const response = await getFromPixabay(searchQuery, actualPage);
-      console.log('57. response', response);
-      console.log('58. searchQuery:', searchQuery, 'actualPage:', actualPage);
       if (response) {
-        console.log('60. response:', response);
         setPictures(p => [...p, ...response.pictures]);
         setTotalHits(response.totalHits);
         setIsLoader(false);
         setTotalPages(response.totalPages);
-        console.log(
-          '67. states after response (pictures, totalHits, isLoader, totalPages):',
-          pictures,
-          totalHits,
-          isLoader,
-          totalPages
-        );
         if (response.pictures.length === 0) Notify.warning('No pictures found');
 
         if (response.pictures.length > 0 && actualPage === 1)
@@ -115,162 +81,25 @@ export const App = () => {
     setActualPage(p => p + 1);
   };
 
-  // render() {
-
   return (
     <div className={css.App}>
       <Searchbar onSubmit={handleSubmitForm} />
-      {console.log('113. after "Searchbar"')}
-      {console.log(
-        'STATE: actualPage:',
-        actualPage,
-        ', pictures:',
-        pictures,
-        ', isModal:',
-        isModal,
-        ', isLoader:',
-        isLoader,
-        ', searchQuery:',
-        searchQuery,
-        ', totalHits:',
-        totalHits,
-        ', totalPages:',
-        totalPages,
-        ', bigPictureUrl:',
-        bigPictureUrl,
-        ', bigPictureTags:',
-        bigPictureTags
-      )}
+
       {totalHits > 0 && (
-        <>
-          <ImageGallery pictures={pictures} onClick={handleModalPict} />
-          {console.log('118. after "ImageGallery"')}
-          {console.log(
-            'STATE: actualPage:',
-            actualPage,
-            ', pictures:',
-            pictures,
-            ', isModal:',
-            isModal,
-            ', isLoader:',
-            isLoader,
-            ', searchQuery:',
-            searchQuery,
-            ', totalHits:',
-            totalHits,
-            ', totalPages:',
-            totalPages,
-            ', bigPictureUrl:',
-            bigPictureUrl,
-            ', bigPictureTags:',
-            bigPictureTags
-          )}
-        </>
+        <ImageGallery pictures={pictures} onClick={handleModalPict} />
       )}
-      {isLoader && (
-        <>
-          <Loader />
-          {console.log('125. after "Loader"')}
-          {console.log(
-            'STATE: actualPage:',
-            actualPage,
-            ', pictures:',
-            pictures,
-            ', isModal:',
-            isModal,
-            ', isLoader:',
-            isLoader,
-            ', searchQuery:',
-            searchQuery,
-            ', totalHits:',
-            totalHits,
-            ', totalPages:',
-            totalPages,
-            ', bigPictureUrl:',
-            bigPictureUrl,
-            ', bigPictureTags:',
-            bigPictureTags
-          )}
-        </>
-      )}
+      {isLoader && <Loader />}
 
       {totalHits > 0 && actualPage < totalPages && (
-        <>
-          <ButtonLoadMore btnLoadMore={handleBtnLoadMore} />
-          {console.log('133. after "ButtonLoadMore"')}
-          {console.log(
-            'STATE: actualPage:',
-            actualPage,
-            ', pictures:',
-            pictures,
-            ', isModal:',
-            isModal,
-            ', isLoader:',
-            isLoader,
-            ', searchQuery:',
-            searchQuery,
-            ', totalHits:',
-            totalHits,
-            ', totalPages:',
-            totalPages,
-            ', bigPictureUrl:',
-            bigPictureUrl,
-            ', bigPictureTags:',
-            bigPictureTags
-          )}
-        </>
+        <ButtonLoadMore btnLoadMore={handleBtnLoadMore} />
       )}
 
       {isModal && (
-        <>
-          <Modal
-            onClose={toggleModalPict}
-            bigPictureUrl={bigPictureUrl}
-            bigPictureTags={bigPictureTags}
-          />
-          {console.log('145. after "Modal"')}
-          {console.log(
-            'STATE: actualPage:',
-            actualPage,
-            ', pictures:',
-            pictures,
-            ', isModal:',
-            isModal,
-            ', isLoader:',
-            isLoader,
-            ', searchQuery:',
-            searchQuery,
-            ', totalHits:',
-            totalHits,
-            ', totalPages:',
-            totalPages,
-            ', bigPictureUrl:',
-            bigPictureUrl,
-            ', bigPictureTags:',
-            bigPictureTags
-          )}
-        </>
-      )}
-
-      {console.log(
-        'STATE: actualPage:',
-        actualPage,
-        ', pictures:',
-        pictures,
-        ', isModal:',
-        isModal,
-        ', isLoader:',
-        isLoader,
-        ', searchQuery:',
-        searchQuery,
-        ', totalHits:',
-        totalHits,
-        ', totalPages:',
-        totalPages,
-        ', bigPictureUrl:',
-        bigPictureUrl,
-        ', bigPictureTags:',
-        bigPictureTags
+        <Modal
+          onClose={toggleModalPict}
+          bigPictureUrl={bigPictureUrl}
+          bigPictureTags={bigPictureTags}
+        />
       )}
     </div>
   );
